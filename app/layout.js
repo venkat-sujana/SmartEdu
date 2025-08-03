5c44640138d25ece9ab7d7865ccbe4221df599a1
venkat-sujana <venkat.bvp34@gmail.com>
2025-08-03 06:53:30 +0530

    fix: navbar path for dashboard page

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import SessionProviderWrapper from "./components/SessionProviderWrapper";
 import Navbar from '../../../components/Navbar';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
