7f706ca3a20bf351b85f597538d56d43a62e0370
venkat-sujana <venkat.bvp34@gmail.com>
2026-06-13 21:02:56 +0530

    pwa error fix

--- src/app/logout/page.jsx
+++ src/app/logout/page.jsx
@@ -1,12 +1,7 @@
+//src/app/logout/page.jsx
 'use client';
-export default function LogoutSuccessPage() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-red-50">
-      <h1 className="text-3xl font-bold text-red-600 mb-4">Logged Out 👋</h1>
-      <p className="text-gray-700">You have been successfully logged out.</p>
-    </div>
-  )
-}
+
+import "./globals.css";
 
 export const metadata = {
   title: "OSRA",
@@ -19,3 +14,11 @@ export const metadata = {
     apple: "/images/osra-6.png",
   },
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
